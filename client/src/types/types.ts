309572e605b27fd3d0ecd1315c17a852de367024
dai <hiroshi01282002@gmail.com>
2025-07-17 17:57:14 +0900

    [Feature] ソートの型定義修正_#45
    - 各モデルごとに定義する配列から利用するようにする

--- client/src/types/types.ts
+++ client/src/types/types.ts
@@ -21,20 +21,3 @@ export type Label = {
   label: string;
   id: string;
 };
-
-export type FilterableField = {
-  key: string;
-  label: string;
-  type: "string" | "number" | "Date" | "select";
-};
-
-export type FilterOperator = "equals" | "contains" | "gte" | "lte";
-export type FilterCondition = FilterableField & {
-  value: string | number | Date;
-  operator: FilterOperator;
-  logic?: "AND" | "OR";
-};
-
-export type SortCondition = FilterableField & {
-  asc: boolean | null;
-};
