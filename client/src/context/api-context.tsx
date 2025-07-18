ecae6d3904910640f2616caa866afb90cf65e4b2
dai <hiroshi01282002@gmail.com>
2025-07-18 16:06:04 +0900

    [Feature] デプロイ準備

--- client/src/context/api-context.tsx
+++ client/src/context/api-context.tsx
@@ -19,7 +19,7 @@ export const useApi = () => {
   const api = axios.create({
     baseURL: import.meta.env.VITE_API_BASE_URL,
     withCredentials: true,
-    timeout: 10000,
+    // timeout: 10000,
   });
 
   // アクセストークン付与
