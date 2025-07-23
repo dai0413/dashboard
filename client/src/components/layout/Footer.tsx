199bcde99c428a2c64ceab006f67c863f91dcdf7
dai <hiroshi01282002@gmail.com>
2025-07-23 14:03:44 +0900

    [Feature] フッター修正_#48
    - スマホ時はメニュー

--- client/src/components/layout/Footer.tsx
+++ client/src/components/layout/Footer.tsx
@@ -1,35 +1,79 @@
+import {
+  HomeIcon,
+  UserCircleIcon,
+  QueueListIcon,
+  TableCellsIcon,
+} from "@heroicons/react/24/solid";
+import { APP_ROUTES } from "../../lib/appRoutes";
+import { useNavigate } from "react-router-dom";
+
 const Footer = () => {
+  const navigate = useNavigate();
+
+  const iconClass = "w-6 h-6 group-hover:text-blue-600";
+
+  const BottomMenuItems = [
+    {
+      to: APP_ROUTES.PLAYER,
+      icon: <HomeIcon className={iconClass} />,
+      text: "ホーム",
+    },
+    {
+      to: APP_ROUTES.TRANSFER,
+      icon: <QueueListIcon className={iconClass} />,
+      text: "移籍",
+    },
+    {
+      to: APP_ROUTES.INJURY,
+      icon: <TableCellsIcon className={iconClass} />,
+      text: "怪我",
+    },
+    {
+      to: APP_ROUTES.ME,
+      icon: <UserCircleIcon className={iconClass} />,
+      text: "プロフィール",
+    },
+  ];
+
   return (
-    <footer className="text-gray-600 body-font">
-      <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
-        <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            className="w-10 h-10 text-white p-2 bg-green-500 rounded-full"
-            viewBox="0 0 24 24"
-          >
-            <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-          </svg>
-          <span className="ml-3 text-xl">DASHBOARD</span>
-        </a>
-        <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
-          © 2025 DASHBOARD —
-          <a
-            href="https://github.com/dai0413/"
-            className="text-gray-600 ml-1"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            @dai0413
-          </a>
-        </p>
-      </div>
-    </footer>
+    <>
+      {/* PC 用フッター（md以上で表示） */}
+      <footer className="hidden md:block text-gray-600 body-font">
+        <div className="container px-5 py-4 mx-auto flex items-center">
+          <p className="text-sm text-gray-500">
+            © 2025 DASHBOARD —
+            <a
+              href="https://github.com/dai0413/"
+              className="text-gray-600 ml-1"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              @dai0413
+            </a>
+          </p>
+        </div>
+      </footer>
+
+      <footer>
+        <div className="block md:hidden fixed bottom-0 left-0 z-50 w-full h-16 bg-white border-t border-gray-200">
+          <div className="grid h-full max-w-lg grid-cols-4 mx-auto font-medium">
+            {BottomMenuItems.map(({ to, icon, text }) => (
+              <button
+                key={to}
+                type="button"
+                onClick={() => navigate(to)}
+                className="inline-flex flex-col items-center justify-center px-5 group cursor-pointer"
+              >
+                {icon}
+                <span className="text-sm text-gray-500 group-hover:text-blue-600">
+                  {text}
+                </span>
+              </button>
+            ))}
+          </div>
+        </div>
+      </footer>
+    </>
   );
 };
 
