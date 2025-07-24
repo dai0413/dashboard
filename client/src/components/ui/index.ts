7acd64a1c61fa2912847e7e7b1e1ea150450ab31
dai <hiroshi01282002@gmail.com>
2025-07-24 17:44:04 +0900

    [Feature] フルスクリーンローディング表示_#54

--- client/src/components/ui/index.ts
+++ client/src/components/ui/index.ts
@@ -1,3 +1,4 @@
 export { default as Arrow } from "./Arrow";
 export { default as Modal } from "./Modal";
 export { default as DropDownMenu } from "./DropDownMenu";
+export { default as FullScreenLoader } from "./FullScreenLoader";
