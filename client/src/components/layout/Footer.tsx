678b3213fda43e782e708c57ad238f6dd8a6979b
dai <hiroshi01282002@gmail.com>
2025-07-24 17:40:21 +0900

    [Feature] メニューに必要なitem一覧作成_#54
    - header用
    - footer用
    - summmaryページのタブ用

--- client/src/components/layout/Footer.tsx
+++ client/src/components/layout/Footer.tsx
@@ -1,40 +1,7 @@
-import {
-  HomeIcon,
-  UserCircleIcon,
-  QueueListIcon,
-  TableCellsIcon,
-} from "@heroicons/react/24/solid";
-import { APP_ROUTES } from "../../lib/appRoutes";
-import { useNavigate } from "react-router-dom";
+import { BottomMenuItems } from "../../constants/menuItems";
+import { IconButton } from "../buttons";
 
 const Footer = () => {
-  const navigate = useNavigate();
-
-  const iconClass = "w-6 h-6 group-hover:text-blue-600";
-
-  const BottomMenuItems = [
-    {
-      to: APP_ROUTES.PLAYER,
-      icon: <HomeIcon className={iconClass} />,
-      text: "ホーム",
-    },
-    {
-      to: APP_ROUTES.TRANSFER,
-      icon: <QueueListIcon className={iconClass} />,
-      text: "移籍",
-    },
-    {
-      to: APP_ROUTES.INJURY,
-      icon: <TableCellsIcon className={iconClass} />,
-      text: "怪我",
-    },
-    {
-      to: APP_ROUTES.ME,
-      icon: <UserCircleIcon className={iconClass} />,
-      text: "プロフィール",
-    },
-  ];
-
   return (
     <>
       {/* PC 用フッター（md以上で表示） */}
@@ -58,17 +25,15 @@ const Footer = () => {
         <div className="block md:hidden fixed bottom-0 left-0 z-50 w-full h-16 bg-white border-t border-gray-200">
           <div className="grid h-full max-w-lg grid-cols-4 mx-auto font-medium">
             {BottomMenuItems.map(({ to, icon, text }) => (
-              <button
-                key={to}
-                type="button"
-                onClick={() => navigate(to)}
-                className="inline-flex flex-col items-center justify-center px-5 group cursor-pointer"
-              >
-                {icon}
-                <span className="text-sm text-gray-500 group-hover:text-blue-600">
-                  {text}
-                </span>
-              </button>
+              <IconButton
+                key={text}
+                icon={icon}
+                text={text}
+                color="gray"
+                to={to}
+                direction="vertical"
+                className="hover:text-green-500"
+              />
             ))}
           </div>
         </div>
---
+++ client/src/constants/menuItems.tsx
@@ -0,0 +1,59 @@
+import { APP_ROUTES } from "../lib/appRoutes";
+import { IconButtonProps } from "../components/buttons/IconButton";
+
+export const BottomMenuItems: IconButtonProps[] = [
+  {
+    to: APP_ROUTES.HOME,
+    icon: "home",
+    text: "ホーム",
+  },
+  {
+    to: APP_ROUTES.TRANSFER,
+    icon: "transfer",
+    text: "移籍",
+  },
+  {
+    to: APP_ROUTES.INJURY,
+    icon: "injury",
+    text: "怪我",
+  },
+  {
+    to: APP_ROUTES.ME,
+    icon: "my-page",
+    text: "マイページ",
+  },
+];
+
+export const SPMenuItems: IconButtonProps[] = [
+  {
+    icon: "transfer",
+    to: APP_ROUTES.TRANSFER,
+    text: "移籍",
+  },
+  {
+    icon: "injury",
+    to: APP_ROUTES.INJURY,
+    text: "怪我",
+  },
+  {
+    icon: "my-page",
+    to: APP_ROUTES.INJURY,
+    text: "マイページ",
+  },
+];
+
+export const PlayerTabItems: IconButtonProps[] = [
+  {
+    icon: "transfer",
+    text: "移籍",
+  },
+  {
+    icon: "injury",
+    text: "怪我",
+  },
+  {
+    icon: "nationality",
+    text: "代表歴",
+    className: "cursor-not-allowed",
+  },
+];
