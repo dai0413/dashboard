aa0d50bc8937402e86baa24e4b78e932fb142747
dai <hiroshi01282002@gmail.com>
2025-08-12 17:24:41 +0900

    [Feature] no-number作成_#77

---
+++ server/services/index.js
@@ -0,0 +1,9 @@
+const getCurrentPlayersByTeamService = require("./Current");
+const getCurrentLoanPlayersByTeamService = require("./CurrentLoan");
+const getNoNumberService = require("./NoNumber");
+
+module.exports = {
+  getCurrentPlayersByTeamService,
+  getCurrentLoanPlayersByTeamService,
+  getNoNumberService,
+};
