344d3f00d3c57aebafd2b628d5fb6c6bfc874096
dai <hiroshi01282002@gmail.com>
2025-08-20 10:44:51 +0900

    [Fix] 軽微な修正
    - summaryからの編集時初期データ
    - clenanData配列対応

--- client/src/utils/cleanData.ts
+++ client/src/utils/cleanData.ts
@@ -1,19 +1,27 @@
 import { FormTypeMap } from "../types/models";
 
 export const cleanData = <T extends keyof FormTypeMap>(
-  data: FormTypeMap[T]
+  data: FormTypeMap[T] | FormTypeMap[T][]
 ) => {
-  const cleanedData: Partial<FormTypeMap[T]> = {};
+  const cleaner = (data: FormTypeMap[T]): Partial<FormTypeMap[T]> => {
+    const cleanedData: Partial<FormTypeMap[T]> = {};
 
-  Object.entries(data).forEach(([key, value]) => {
-    if (Array.isArray(value)) {
-      cleanedData[key as keyof FormTypeMap[T]] = value.filter(
-        (v) => v && v.trim() !== ""
-      ) as FormTypeMap[T][keyof FormTypeMap[T]];
-    } else {
-      cleanedData[key as keyof FormTypeMap[T]] = value;
-    }
-  });
+    Object.entries(data).forEach(([key, value]) => {
+      if (Array.isArray(value)) {
+        cleanedData[key as keyof FormTypeMap[T]] = value.filter(
+          (v) => v && v.trim() !== ""
+        ) as FormTypeMap[T][keyof FormTypeMap[T]];
+      } else {
+        cleanedData[key as keyof FormTypeMap[T]] = value;
+      }
+    });
 
-  return cleanedData;
+    return cleanedData;
+  };
+
+  if (Array.isArray(data)) {
+    return data.map((d) => cleaner(d));
+  }
+
+  return cleaner(data);
 };
