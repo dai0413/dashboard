776faf9149888d5ec60e6858e657921b0c3747f3
dai <hiroshi01282002@gmail.com>
2025-08-20 10:41:52 +0900

    [Feature] 多数データ編集フィールド_#86
    - テーブル内で編集できるようなフィールド

---
+++ client/src/components/modals/Form/ManyField.tsx
@@ -0,0 +1,133 @@
+import { FieldDefinition } from "../../../types/form";
+import { FormTypeMap } from "../../../types/models";
+import { useOptions } from "../../../context/options-provider";
+import { Table } from "../../table";
+import { RenderField } from "./Field";
+import { useState } from "react";
+import { TableHeader } from "../../../types/types";
+import { IconTextButton } from "../../buttons";
+import { useQuery } from "../../../context/query-context";
+import { useForm } from "../../../context/form-context";
+
+type RenderFieldProps<_T extends keyof FormTypeMap> = {
+  isTableOpen: boolean;
+  toggleTableOpen: () => void;
+};
+
+export const RenderManyField = <T extends keyof FormTypeMap>({
+  isTableOpen,
+  toggleTableOpen,
+}: RenderFieldProps<T>) => {
+  const {
+    many,
+    steps: { currentStep },
+  } = useForm<T>();
+
+  const { getOptions } = useOptions();
+  const { page, setPage } = useQuery();
+  type Focus = {
+    field: FieldDefinition<T>;
+    rowIndex: number;
+  };
+  const [focus, setFocus] = useState<Focus | null>(null);
+
+  const fields = many?.formSteps[currentStep].fields as FieldDefinition<
+    keyof FormTypeMap
+  >[];
+
+  const formData = focus && many?.formData[focus.rowIndex];
+
+  const handleSetPage = (p: number) => setPage("formPage", p);
+
+  if (focus?.field.type === "table" && formData) {
+    if (isTableOpen && focus)
+      return (
+        <RenderField
+          field={focus.field}
+          formData={formData}
+          handleFormData={(key, value) =>
+            many?.handleFormData(focus.rowIndex, key, value)
+          }
+        />
+      );
+  }
+
+  const headers = fields
+    ? fields?.map((field) => ({
+        label: field.label,
+        field: field.key as string,
+        width: field.width,
+      }))
+    : [];
+
+  const requiredField = fields.filter((f) => f.required).map((f) => f.key);
+
+  return (
+    <>
+      <Table
+        data={many?.formData || []}
+        headers={headers}
+        renderFieldCell={(
+          header: TableHeader,
+          formData: FormTypeMap[T],
+          rowIndex: number
+        ) => {
+          const field = fields?.find((f) => f.key === header.field);
+          if (!field) return null;
+
+          const value = formData[field.key] as string | number | Date;
+
+          if (field.type === "table") {
+            return (
+              <>
+                <button
+                  type="button"
+                  className={`text-gray-500 bg-green-100 hover:bg-green-200 font-medium rounded-full text-sm px-3 py-1 text-center mx-2`}
+                  onClick={() => {
+                    toggleTableOpen();
+                    setFocus({
+                      field: field,
+                      rowIndex: rowIndex,
+                    });
+                  }}
+                >
+                  編集
+                </button>
+                <>
+                  {getOptions(field.key as string, false, false).find(
+                    (f) => f.key === value
+                  )?.label || "未選択"}
+                </>
+              </>
+            );
+          } else
+            return (
+              <RenderField
+                field={field}
+                formData={formData}
+                handleFormData={(key, value) =>
+                  many?.handleFormData(rowIndex, key, value)
+                }
+              />
+            );
+        }}
+        currentPage={page.formPage}
+        onPageChange={(p: number) => setPage("formPage", p)}
+        itemsPerPage={10}
+        edit={true}
+        deleteOnClick={many?.deleteFormDatas}
+        selectedKey={requiredField}
+      />
+
+      <IconTextButton
+        icon="add"
+        color="blue"
+        onClick={() => {
+          many?.addFormDatas(handleSetPage);
+        }}
+      >
+        データ追加
+      </IconTextButton>
+    </>
+  );
+};
