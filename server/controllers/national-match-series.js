5ac5e9ee0a131ff3242fbc7c0df57b19f48594c0
dai <hiroshi01282002@gmail.com>
2025-09-11 12:27:56 +0900

    [Feature] national-match-seriesのダウンロードを追加

--- server/controllers/national-match-series.js
+++ server/controllers/national-match-series.js
@@ -99,10 +99,37 @@ const deleteNationalMatchSeries = async (req, res) => {
   res.status(StatusCodes.OK).json({ message: "削除しました" });
 };
 
+const downloadItems = async (req, res) => {
+  try {
+    const items = await NationalMatchSeries.find();
+    if (items.length === 0) {
+      return res.status(404).json({ message: "データがありません" });
+    }
+
+    const header = `"_id","name","abbr","country","age_group","joined_at","left_at","urls"\n`;
+
+    const csvContent = items
+      .map((item) => {
+        return `"${item._id}","${item.name}","${item.abbr}","${item.country}","${item.age_group}","${item.joined_at}","${item.left_at}","${item.urls}"`;
+      })
+      .join("\n");
+
+    res.header("Content-Type", "text/csv; charset=utf-8");
+    res.attachment("items.csv");
+    res.status(StatusCodes.OK).send("\uFEFF" + header + csvContent); // 先頭にBOMをつけてExcel文字化け防止
+  } catch (err) {
+    console.error("CSV出力エラー:", err);
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ message: "CSV出力に失敗しました" });
+  }
+};
+
 module.exports = {
   getAllNationalMatchSeries,
   createNationalMatchSeries,
   getNationalMatchSeries,
   updateNationalMatchSeries,
   deleteNationalMatchSeries,
+  downloadItems,
 };
