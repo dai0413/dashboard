586b771deb1a26c621e6a2542e685dd3479ea415
dai <hiroshi01282002@gmail.com>
2025-10-08 21:31:45 +0900

    [Feature] install package
    - zod : 型定義, バリデーション
    - typescript

---
+++ server/jest.config.js
@@ -0,0 +1,13 @@
+const { createDefaultPreset } = require("ts-jest");
+
+const tsJestTransformCfg = createDefaultPreset().transform;
+
+/** @type {import("jest").Config} **/
+module.exports = {
+  transform: {
+    ...tsJestTransformCfg,
+  },
+  preset: "ts-jest",
+  testEnvironment: "node",
+  setupFilesAfterEnv: ["<rootDir>/test-utils/setup.ts"],
+};
