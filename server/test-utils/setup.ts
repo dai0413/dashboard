231e6ca29729ccd5795693e621547c045a0b5df6
dai <hiroshi01282002@gmail.com>
2025-10-08 21:40:03 +0900

    [Feature] apiテスト player_#110

---
+++ server/test-utils/setup.ts
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+import { MongoMemoryServer } from "mongodb-memory-server";
+
+let mongo: MongoMemoryServer;
+
+// 認証ミドルウェアをモック
+jest.mock("../middleware/auth", () => {
+  return (req: any, res: any, next: any) => {
+    req.user = { id: "test-user" };
+    next();
+  };
+});
+
+beforeAll(async () => {
+  mongo = await MongoMemoryServer.create();
+  const uri = mongo.getUri();
+  await mongoose.connect(uri);
+});
+
+afterAll(async () => {
+  await mongoose.connection.dropDatabase();
+  await mongoose.connection.close();
+  await mongo.stop();
+});
