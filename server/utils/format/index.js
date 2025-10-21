683af877bfdff5d7f6cbf6125d89bd60404d829b
dai <hiroshi01282002@gmail.com>
2025-10-21 17:11:31 +0900

    [Feature] server/utils/formatをtsに変更_#114

--- server/utils/format/index.js
+++
@@ -1,6 +0,0 @@
-import { transfer as formatTransfer } from "./transfer.js";
-import { match as formatMatch } from "./match.js";
-import { injury as formatInjury } from "./injury.js";
-import { nationalCallup as formatNationalCallup } from "./national-callup.js";
-
-export { formatTransfer, formatMatch, formatInjury, formatNationalCallup };
---
+++ server/utils/format/transfer.ts
@@ -0,0 +1,32 @@
+import z from "zod";
+
+import {
+  TransferResponseSchema,
+  TransferPopulatedSchema,
+} from "../../../shared/schemas/transfer.schema.ts";
+
+const transfer = (
+  data: z.infer<typeof TransferPopulatedSchema>
+): z.infer<typeof TransferResponseSchema> => {
+  const { from_team, to_team, from_team_name, to_team_name, ...rest } = data;
+
+  const from_team_obj = from_team
+    ? from_team
+    : from_team_name
+    ? { team: from_team_name }
+    : undefined;
+
+  const to_team_obj = to_team
+    ? to_team
+    : to_team_name
+    ? { team: to_team_name }
+    : undefined;
+
+  return {
+    ...rest,
+    from_team: from_team_obj,
+    to_team: to_team_obj,
+  };
+};
+
+export { transfer };
