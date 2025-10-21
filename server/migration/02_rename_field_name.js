dfa08e75bb7df2f46dd925936fdf2fa4fdc1b93e
dai <hiroshi01282002@gmail.com>
2025-10-21 17:12:08 +0900

    [Feature] server/migrationをtsに変更_#114

--- server/migration/02_rename_field_name.js
+++ server/migration/02_rename_field_name.js
@@ -1,10 +1,7 @@
-import path from "path";
-import dotenv from "dotenv";
-dotenv.config({
-  path: path.resolve(process.cwd(), "../.env"),
-});
-
 import { mongoose } from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
 const mongoUri = process.env.MONGODB_URI;
 
 (async () => {
