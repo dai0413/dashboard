ef831211e545bc8922cc1556361cae07aad54336
dai <hiroshi01282002@gmail.com>
2025-12-09 16:18:07 +0900

    [Fix] 不要ファイル削除
    - sharedに移行済み

--- client/src/utils/createOption/Enum/result.ts
+++
@@ -1,7 +0,0 @@
-import { OptionArray } from "../../../types/option";
-
-export const result = (): OptionArray => [
-  { key: "home", label: "ホーム" },
-  { key: "away", label: "アウェイ" },
-  { key: "draw", label: "ドロー" },
-];
--- client/src/utils/createOption/Enum/status.ts
+++
@@ -1,7 +0,0 @@
-import { OptionArray } from "../../../types/option";
-
-export const status = (): OptionArray => [
-  { key: "joined", label: "参加" },
-  { key: "declined", label: "事前辞退" },
-  { key: "withdrawn", label: "途中離脱" },
-];
