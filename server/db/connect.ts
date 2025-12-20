badde6f870c336cde509c3aab45ad09d196f1441
dai <hiroshi01282002@gmail.com>
2025-12-20 21:15:30 +0900

    [Fix] db接続

--- server/db/connect.ts
+++ server/db/connect.ts
@@ -1,22 +1,5 @@
 import mongoose from "mongoose";
 
-let cached = (global as any).mongoose;
-
-if (!cached) {
-  cached = (global as any).mongoose = { conn: null, promise: null };
-}
-
-export default async function connectDB(uri: string) {
-  if (cached.conn) return cached.conn;
-
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(uri, {
-      maxPoolSize: 5,
-      serverSelectionTimeoutMS: 5000,
-      socketTimeoutMS: 20000,
-    });
-  }
-
-  cached.conn = await cached.promise;
-  return cached.conn;
+export default function connectDB(url: string) {
+  return mongoose.connect(url, {});
 }
