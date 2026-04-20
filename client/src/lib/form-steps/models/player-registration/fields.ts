66582882dd1f8f484a51fc07c5864e1655cab6ad
dai0413 <hiroshi01282002@gmail.com>
2026-04-20 22:22:53 +0900

    [Refactor] form-steps修正 player-registration #172

---
+++ client/src/lib/form-steps/models/player-registration/fields.ts
@@ -0,0 +1,104 @@
+import { FormFieldDefinition } from "../../../../types/form";
+import { ModelType } from "../../../../types/models";
+
+type BaseModel = ModelType.PLAYER_REGISTRATION;
+type Key = FormFieldDefinition<BaseModel>["key"];
+
+export const fieldMap: Record<Key, FormFieldDefinition<BaseModel>> = {
+  season: {
+    key: "season",
+    label: "大会シーズン",
+    fieldType: "table",
+    valueType: "option",
+    required: true,
+  },
+  player: {
+    key: "player",
+    label: "選手",
+    fieldType: "table",
+    valueType: "option",
+    required: true,
+  },
+  team: {
+    key: "team",
+    label: "チーム",
+    fieldType: "table",
+    valueType: "option",
+    required: true,
+  },
+  registration_type: {
+    key: "registration_type",
+    label: "登録・抹消",
+    fieldType: "select",
+    valueType: "option",
+  },
+  date: {
+    key: "date",
+    label: "日付",
+    fieldType: "input",
+    valueType: "date",
+  },
+  number: {
+    key: "number",
+    label: "背番号",
+    fieldType: "input",
+    valueType: "number",
+  },
+  position_group: {
+    key: "position_group",
+    label: "ポジション",
+    fieldType: "select",
+    valueType: "option",
+  },
+  name: {
+    key: "name",
+    label: "名前",
+    fieldType: "input",
+    valueType: "text",
+  },
+  en_name: {
+    key: "en_name",
+    label: "英名",
+    fieldType: "input",
+    valueType: "text",
+  },
+  height: {
+    key: "height",
+    label: "身長",
+    fieldType: "input",
+    valueType: "number",
+  },
+  weight: {
+    key: "weight",
+    label: "体重",
+    fieldType: "input",
+    valueType: "number",
+  },
+  isTypeTwo: {
+    key: "isTypeTwo",
+    label: "2種登録",
+    fieldType: "input",
+    valueType: "boolean",
+  },
+  isSpecialDesignation: {
+    key: "isSpecialDesignation",
+    label: "特別指定",
+    fieldType: "input",
+    valueType: "boolean",
+  },
+  homegrown: {
+    key: "homegrown",
+    label: "ホームグロウン",
+    fieldType: "input",
+    valueType: "boolean",
+  },
+  note: {
+    key: "note",
+    label: "メモ",
+    fieldType: "input",
+    valueType: "text",
+  },
+};
+
+export const getFields = (keys: (keyof typeof fieldMap)[]) =>
+  keys.map((key) => fieldMap[key]);
---
+++ client/src/lib/form-steps/models/player-registration/forms/single.ts
@@ -0,0 +1,97 @@
+import { FormStep, FormUpdatePair, StepType } from "../../../../../types/form";
+import { ModelType } from "../../../../../types/models";
+import { createConfirmationStep } from "../../../confirmationStep";
+import { readItemBase } from "../../../../api";
+import { API_PATHS } from "@dai0413/myorg-shared";
+import { convert } from "../../../../convert/DBtoGetted";
+import { currentTransfer } from "../../../utils/onChange/currentTransfer";
+import { getFields } from "../fields";
+
+type BaseModel = ModelType.PLAYER_REGISTRATION;
+const baseModel = ModelType.PLAYER_REGISTRATION;
+
+export const single: FormStep<ModelType.PLAYER_REGISTRATION>[] = [
+  {
+    stepLabel: "大会シーズン選択",
+    type: StepType.FORM,
+    modelType: baseModel,
+    fields: getFields(["season"]),
+  },
+  {
+    stepLabel: "選手選択",
+    type: StepType.FORM,
+    modelType: baseModel,
+    fields: getFields(["player"]),
+    onChange: async (formData, api) => {
+      let obj: FormUpdatePair = [];
+      if (!formData.player) return [];
+
+      // name, en_name の設定
+      const res = await readItemBase({
+        apiInstance: api,
+        backendRoute: API_PATHS.PLAYER.DETAIL(formData.player),
+        returnResponse: true,
+      });
+
+      const { name, en_name } = convert(ModelType.PLAYER, res.data);
+
+      if (name) {
+        obj.push({
+          key: "name",
+          value: name,
+        });
+      }
+
+      if (en_name) {
+        obj.push({
+          key: "en_name",
+          value: en_name,
+        });
+      }
+
+      // teamの設定
+      const { to_team } = await currentTransfer({ formData, api });
+      if (to_team) {
+        obj.push({
+          key: "team",
+          value: to_team,
+        });
+      }
+
+      return obj;
+    },
+  },
+  {
+    stepLabel: "チーム選択",
+    type: StepType.FORM,
+    modelType: baseModel,
+    fields: getFields(["team"]),
+  },
+  {
+    stepLabel: "登録or抹消・日付・背番号・POS.・名前・英名・身長・体重を入力",
+    type: StepType.FORM,
+    modelType: baseModel,
+    fields: getFields([
+      "registration_type",
+      "date",
+      "number",
+      "position_group",
+      "name",
+      "en_name",
+      "height",
+      "weight",
+    ]),
+  },
+  {
+    stepLabel: "2種登録・特別指定・HG・メモを入力",
+    type: StepType.FORM,
+    modelType: baseModel,
+    fields: getFields([
+      "isTypeTwo",
+      "isSpecialDesignation",
+      "homegrown",
+      "note",
+    ]),
+  },
+  createConfirmationStep<BaseModel>(baseModel),
+];
