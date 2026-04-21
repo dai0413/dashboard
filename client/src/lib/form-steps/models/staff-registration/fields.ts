4a3b5bddfdee5707abe78d1e52af6809d710af76
dai0413 <hiroshi01282002@gmail.com>
2026-04-21 17:58:38 +0900

    [Refactor] form-steps修正 staff-registration #172

---
+++ client/src/lib/form-steps/models/staff-registration/fields.ts
@@ -0,0 +1,68 @@
+import { FormFieldDefinition } from "../../../../types/form";
+import { ModelType } from "../../../../types/models";
+
+type BaseModel = ModelType.STAFF_REGISTRATION;
+type Key = FormFieldDefinition<BaseModel>["key"];
+
+export const fieldMap: Record<Key, FormFieldDefinition<BaseModel>> = {
+  season: {
+    key: "season",
+    label: "大会シーズン",
+    fieldType: "table",
+    valueType: "option",
+    required: true,
+  },
+  staff: {
+    key: "staff",
+    label: "スタッフ",
+    fieldType: "table",
+    valueType: "option",
+    required: true,
+  },
+  team: {
+    key: "team",
+    label: "チーム",
+    fieldType: "table",
+    valueType: "option",
+    required: true,
+  },
+  registration_type: {
+    key: "registration_type",
+    label: "登録・抹消",
+    fieldType: "select",
+    valueType: "option",
+  },
+  date: {
+    key: "date",
+    label: "日付",
+    fieldType: "input",
+    valueType: "date",
+  },
+  role: {
+    key: "role",
+    label: "役割",
+    fieldType: "input",
+    valueType: "text",
+  },
+  name: {
+    key: "name",
+    label: "名前",
+    fieldType: "input",
+    valueType: "text",
+  },
+  en_name: {
+    key: "en_name",
+    label: "英名",
+    fieldType: "input",
+    valueType: "text",
+  },
+  note: {
+    key: "note",
+    label: "メモ",
+    fieldType: "input",
+    valueType: "text",
+  },
+};
+
+export const getFields = (keys: (keyof typeof fieldMap)[]) =>
+  keys.map((key) => fieldMap[key]);
---
+++ client/src/lib/form-steps/models/staff-registration/forms/single.ts
@@ -0,0 +1,84 @@
+import { FormStep, FormUpdatePair, StepType } from "../../../../../types/form";
+import { ModelType } from "../../../../../types/models";
+import { createConfirmationStep } from "../../../confirmationStep";
+import { readItemBase } from "../../../../api";
+import { API_PATHS } from "@dai0413/myorg-shared";
+import { convert } from "../../../../convert/DBtoGetted";
+import { currentTransfer } from "../../../utils/onChange/currentTransfer";
+import { getFields } from "../fields";
+
+type BaseModel = ModelType.STAFF_REGISTRATION;
+const baseModel = ModelType.STAFF_REGISTRATION;
+
+export const single: FormStep<ModelType.STAFF_REGISTRATION>[] = [
+  {
+    stepLabel: "大会シーズン選択",
+    type: StepType.FORM,
+    modelType: baseModel,
+    fields: getFields(["season"]),
+  },
+  {
+    stepLabel: "スタッフ選択",
+    type: StepType.FORM,
+    modelType: baseModel,
+    fields: getFields(["staff"]),
+    onChange: async (formData, api) => {
+      let obj: FormUpdatePair = [];
+      if (!formData.staff) return [];
+
+      // name, en_name の設定
+      const res = await readItemBase({
+        apiInstance: api,
+        backendRoute: API_PATHS.STAFF.DETAIL(formData.staff),
+        returnResponse: true,
+      });
+
+      const { name, en_name } = convert(ModelType.STAFF, res.data);
+
+      if (name) {
+        obj.push({
+          key: "name",
+          value: name,
+        });
+      }
+
+      if (en_name) {
+        obj.push({
+          key: "en_name",
+          value: en_name,
+        });
+      }
+
+      // teamの設定
+      const { to_team } = await currentTransfer({ formData, api });
+      if (to_team) {
+        obj.push({
+          key: "team",
+          value: to_team,
+        });
+      }
+
+      return obj;
+    },
+  },
+  {
+    stepLabel: "チーム選択",
+    type: StepType.FORM,
+    modelType: baseModel,
+    fields: getFields(["team"]),
+  },
+  {
+    stepLabel: "登録or抹消・日付・役割・名前・英名を入力",
+    type: StepType.FORM,
+    modelType: baseModel,
+    fields: getFields([
+      "registration_type",
+      "date",
+      "role",
+      "name",
+      "en_name",
+      "note",
+    ]),
+  },
+  createConfirmationStep<BaseModel>(baseModel),
+];
