d54ceb47d1bdde5f9ef8ecaac10639f27a1ce8ff
dai0413 <hiroshi01282002@gmail.com>
2026-06-19 15:09:07 +0900

    [Feature] フォームフィールドにローディング表示 #187

--- client/src/components/modals/Form/FormPresenter.tsx
+++ client/src/components/modals/Form/FormPresenter.tsx
@@ -1,14 +1,15 @@
 import { FormHeader } from "./ui/FormHeader";
 import { FormFooter } from "./ui/FormFooter";
-import { StepRenderer } from "./renderers/StepRenderer";
 import { Modal } from "../../ui";
 import { FormController } from "./types";
 import { FormTypeMap } from "../../../types/models";
+import { Loader2 } from "lucide-react";
+import { StepRenderer } from "./renderers/StepRenderer";
 
 export const FormPresenter = <T extends keyof FormTypeMap>(
   props: FormController<T>,
 ) => {
-  const { isOpen, close } = props;
+  const { isOpen, close, isProcessing } = props;
 
   return (
     <Modal
@@ -17,7 +18,15 @@ export const FormPresenter = <T extends keyof FormTypeMap>(
       header={<FormHeader {...props} />}
       footer={<FormFooter {...props} />}
     >
-      <StepRenderer {...props} />
+      {isProcessing ? (
+        <div className="flex items-center justify-center py-16">
+          <div className="bg-gray-50 px-8 py-10 text-center">
+            <Loader2 className="animate-spin w-10 h-10 text-gray-600" />
+          </div>
+        </div>
+      ) : (
+        <StepRenderer {...props} />
+      )}
     </Modal>
   );
 };
