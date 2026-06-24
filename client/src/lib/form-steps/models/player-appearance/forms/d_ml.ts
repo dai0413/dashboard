1b837dc21f452a8e94af73ff2242bef17b6f68fc
dai0413 <hiroshi01282002@gmail.com>
2026-06-24 11:17:37 +0900

    [Feature] position取得追加 #190

--- client/src/lib/form-steps/models/player-appearance/forms/d_ml.ts
+++ client/src/lib/form-steps/models/player-appearance/forms/d_ml.ts
@@ -7,6 +7,12 @@ import { getPreMatchSelect } from "../../../d_ml/preMatchSelectStep";
 import { getDraftData } from "../getDraftData";
 import { From } from "../../../../../types/types";
 import { addPostedDraftData } from "../addPostedDraftData";
+import { ReadDraftDataParams } from "../../../utils/getDraftData/types";
+import { readItemBase } from "../../../../api";
+import { API_PATHS } from "@dai0413/myorg-shared";
+import { Match } from "../../../../../types/models/match";
+import { Team } from "../../../../../types/models/team";
+import { readL_MMap } from "../../../utils/getDraftData/readMap/readL_M";
 
 type BaseModel = ModelType.PLAYER_APPEARANCE;
 const baseModel = ModelType.PLAYER_APPEARANCE;
@@ -20,18 +26,64 @@ export const playerAppearance: FormStep<BaseModel>[] = [
     type: StepType.FORM,
     many: true,
     createFilterConditions: async (args) => setMatchTeam(args.data, args.api),
-    getDraftData: async ({ api, draftData, postedDraftData, metaData }) => {
+    getDraftData: async ({
+      api,
+      data,
+      draftData,
+      postedDraftData,
+      metaData,
+    }) => {
       const url: string = metaData.matchUrl;
-      const match: string[] = metaData.match;
+      const match: string | undefined = data.match;
+      if (!match) return { value: [], label: [] };
+
+      const matchObj = await readItemBase<Match>({
+        apiInstance: api,
+        backendRoute: API_PATHS.MATCH.DETAIL(match),
+      });
+
+      if (!matchObj) return { value: [], label: [] };
+
+      let newDraftData = draftData;
+
+      const date = matchObj.date;
+      const alph = matchObj.home_team.labalph;
+
+      if (date && alph && match) {
+        const params: { date: Date; alph: string; matchId: string } = {
+          date,
+          alph,
+          matchId: match,
+        };
+
+        let res =
+          readL_MMap.positions &&
+          (await readL_MMap.positions(api, { getParams: [params] }));
+
+        const positionDatas = res?.success ? res.data : undefined;
+        for (const positinKey in positionDatas) {
+          const key = positinKey as keyof typeof newDraftData;
+          newDraftData[key] = {
+            ...newDraftData[key],
+            positions: { ...positionDatas[key] },
+          };
+        }
+      }
+
+      const requests: ReadDraftDataParams["requests"] = [
+        {
+          draftDataKey: "playerAppearance",
+          from: From.D_M,
+          params: { url },
+        },
+      ];
 
       return await getDraftData({
         readDraftDataParams: {
           api,
-          draftData,
-          identifiers: match,
-          requests: [
-            { draftDataKey: "match", from: From.D_M, params: { url } },
-          ],
+          draftData: newDraftData,
+          identifiers: [match],
+          requests,
         },
         postedDraftData,
         season: metaData.season,
@@ -50,15 +102,56 @@ export const multiModel: FormStep<BaseModel>[] = [
     many: true,
     getDraftData: async ({ api, draftData, postedDraftData, metaData }) => {
       const cardIds: string[] = metaData.card_ids;
+      const params = (
+        await Promise.all(
+          cardIds.map(async (cardId) => {
+            const value = postedDraftData[cardId];
+            if (!value.match) return;
 
-      return getDraftData({
+            const { date, home_team } = value.match;
+
+            if (!date || !home_team.id) return;
+
+            const team = await readItemBase<Team>({
+              apiInstance: api,
+              backendRoute: API_PATHS.TEAM.DETAIL(home_team.id),
+            });
+
+            if (!team || !team.labalph) return;
+
+            return {
+              date: date,
+              alph: team.labalph,
+              matchId: cardId,
+            };
+          }),
+        )
+      ).filter((d) => typeof d !== "undefined");
+
+      let res =
+        readL_MMap.positions &&
+        (await readL_MMap.positions(api, { getParams: params }));
+
+      const positionDatas = res?.success ? res.data : undefined;
+
+      let newDraftData = draftData;
+
+      for (const positinKey in positionDatas) {
+        const key = positinKey as keyof typeof newDraftData;
+        newDraftData[key] = {
+          ...newDraftData[key],
+          positions: { ...positionDatas[key] },
+        };
+      }
+
+      let gettedDraftData = await getDraftData({
         readDraftDataParams: {
           api,
-          draftData,
+          draftData: newDraftData,
           identifiers: cardIds,
           requests: [
             {
-              draftDataKey: "match",
+              draftDataKey: "playerAppearance",
               from: From.D_M,
               params: { cardId: cardIds },
             },
@@ -67,6 +160,8 @@ export const multiModel: FormStep<BaseModel>[] = [
         postedDraftData,
         season: metaData.season,
       });
+
+      return gettedDraftData;
     },
   },
   bulkBase,
