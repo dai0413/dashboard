33e08ec58fcd5dab76ea15caac06ef3661c955a3
dai0413 <hiroshi01282002@gmail.com>
2026-08-18 10:10:53 +0900

    [Feature] usePositionPanel.ts削除 #220
    - useStatisticsPanelからデータ使用

--- client/src/pages/Summary/Player/hooks/index.ts
+++ client/src/pages/Summary/Player/hooks/index.ts
@@ -1,6 +1,5 @@
 export { useInjuryPanel } from "./useInjuryPanel";
 export { useNationalCallupPanel } from "./useNationalCallupPanel";
 export { usePlayerRegistrationPanel } from "./usePlayerRegistrationPanel";
-export { usePositionPanel } from "./usePositionPanel";
 export { useTransferPanel } from "./useTransferPanel";
 export { useStatisticsPanel } from "./useStatisticsPanel";
--- client/src/pages/Summary/Player/hooks/usePlayerSummary.ts
+++ client/src/pages/Summary/Player/hooks/usePlayerSummary.ts
@@ -1,14 +1,15 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import {
   useInjuryPanel,
   useNationalCallupPanel,
   usePlayerRegistrationPanel,
-  usePositionPanel,
   useStatisticsPanel,
   useTransferPanel,
 } from "./index";
 import { PLAYER_TAB, PlayerTab, UsePlayerSummary } from "../types";
 import { usePlayer } from "../../../../context/models/player";
+import { FormationItem } from "../../../../types/formation";
+import { positionBase } from "../../../../components/formation/positionBase";
 
 export const usePlayerSummary = (id: string): UsePlayerSummary => {
   const {
@@ -29,16 +30,40 @@ export const usePlayerSummary = (id: string): UsePlayerSummary => {
   const { nationalCallups, readNationalCallups } = useNationalCallupPanel();
   const { playerRegistrations, readPlayerRegistrations } =
     usePlayerRegistrationPanel();
-  const { positions, readPositions } = usePositionPanel();
   const { transfers, readTransfers } = useTransferPanel();
   const { statistics, readStatistics } = useStatisticsPanel();
+
+  const positions: FormationItem[] = useMemo(() => {
+    const result: FormationItem[] = [];
+
+    statistics.data.forEach((statistic) => {
+      for (const [key, value] of Object.entries(statistic.positionCounts)) {
+        const point = positionBase[key as keyof typeof positionBase];
+
+        result.push({
+          position: key as FormationItem["position"],
+          centerText: value,
+          label: key,
+          size: 24 + ((value || 0) / statistic.appearances) * 28,
+          color: point.color,
+          tooltip: [
+            { text: key, bold: true },
+            {
+              text: `${value}試合`,
+            },
+          ],
+        });
+      }
+    });
+
+    return result;
+  }, [statistics]);
 
   const readDatas = async (playerId: string) => {
     await Promise.all([
       readinjuries(playerId),
       readNationalCallups(playerId),
       readPlayerRegistrations(playerId),
-      readPositions(playerId),
       readTransfers(playerId),
       readStatistics(playerId),
     ]);
@@ -78,27 +103,32 @@ export const usePlayerSummary = (id: string): UsePlayerSummary => {
       nationalCallup: {
         key: `${selectedTab}`,
         items: nationalCallups.data,
+        isLoading: nationalCallups.isLoading,
         reloadFun: async () => readNationalCallups(id),
       },
 
       position: {
         key: `${selectedTab}`,
         items: positions,
-        reloadFun: async () => readPositions(id),
+        isLoading: statistics.isLoading,
+        reloadFun: async () => readStatistics(id),
       },
       transfer: {
         key: `${selectedTab}`,
         items: transfers.data,
+        isLoading: transfers.isLoading,
         reloadFun: async () => readTransfers(id),
       },
       injury: {
         key: `${selectedTab}`,
         items: injuries.data,
+        isLoading: injuries.isLoading,
         reloadFun: async () => readinjuries(id),
       },
       playerRegistration: {
         key: `${selectedTab}`,
         items: playerRegistrations.data,
+        isLoading: playerRegistrations.isLoading,
         reloadFun: async () => readPlayerRegistrations(id),
       },
       statistics: {
