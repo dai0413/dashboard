3dab831a4417e18c5dc5ff15a08491cbcab62f52
dai0413 <hiroshi01282002@gmail.com>
2026-08-18 14:16:17 +0900

    [Feature] playerStatisticsのフィールドを外部化 #220

---
+++ client/src/lib/fields/playerStatistics.ts
@@ -0,0 +1,113 @@
+import { PlayerStatistic } from "@dai0413/myorg-shared/types/aggregate/player/statistic";
+import { UIFieldDefinition } from "../../types/field";
+import { ColumnType } from "../../types/table";
+
+const convert = (p: PlayerStatistic["player"]): string => {
+  if (p.name) return p.name;
+  if (p.en_name) return p.en_name;
+  return "";
+};
+
+export const playerStatistics: UIFieldDefinition<PlayerStatistic>[] = [
+  {
+    key: "player",
+    filterKey: "player.name",
+    label: "選手",
+    type: "string",
+    filterable: true,
+    sortable: true,
+    displayOnDetail: true,
+    displayOnTable: true,
+    getValueType: ColumnType.CUSTOM,
+    getData: (d) => ({ id: d.player._id, label: convert(d.player) }),
+    width: "100px",
+  },
+  {
+    key: "mainPosition",
+    label: "メイン",
+    type: "string",
+    displayOnTable: true,
+    filterable: true,
+    sortable: true,
+    getValueType: ColumnType.FIELD,
+    field: "mainPosition",
+    width: "50px",
+  },
+  {
+    key: "appearances",
+    label: "試合数",
+    type: "number",
+    displayOnTable: true,
+    filterable: true,
+    sortable: true,
+    getValueType: ColumnType.FIELD,
+    field: "appearances",
+    width: "50px",
+  },
+  {
+    key: "starts",
+    label: "スタメン",
+    type: "number",
+    displayOnTable: true,
+    filterable: true,
+    sortable: true,
+    getValueType: ColumnType.FIELD,
+    field: "starts",
+    width: "50px",
+  },
+  {
+    key: "subs",
+    label: "サブ",
+    type: "number",
+    displayOnTable: true,
+    filterable: true,
+    sortable: true,
+    getValueType: ColumnType.FIELD,
+    field: "subs",
+    width: "50px",
+  },
+  {
+    key: "bench",
+    label: "ベンチ",
+    type: "number",
+    displayOnTable: true,
+    filterable: true,
+    sortable: true,
+    getValueType: ColumnType.FIELD,
+    field: "bench",
+    width: "50px",
+  },
+  {
+    key: "minutes",
+    label: "時間",
+    type: "number",
+    displayOnTable: true,
+    filterable: true,
+    sortable: true,
+    getValueType: ColumnType.FIELD,
+    field: "minutes",
+    width: "50px",
+  },
+  {
+    key: "goals",
+    label: "G",
+    type: "number",
+    displayOnTable: true,
+    filterable: true,
+    sortable: true,
+    getValueType: ColumnType.FIELD,
+    field: "goals",
+    width: "50px",
+  },
+  {
+    key: "assists",
+    label: "A",
+    type: "number",
+    displayOnTable: true,
+    filterable: true,
+    sortable: true,
+    getValueType: ColumnType.FIELD,
+    field: "assists",
+    width: "50px",
+  },
+];
