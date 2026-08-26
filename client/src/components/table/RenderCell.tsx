185818a021248705b08a69b73c24dbb99d876d3c
dai0413 <hiroshi01282002@gmail.com>
2026-08-26 17:38:31 +0900

    [Feature] RenderCellコンポーネントを拡張 #220
    - link対応

--- client/src/components/table/RenderCell.tsx
+++ client/src/components/table/RenderCell.tsx
@@ -2,54 +2,77 @@ import { Link } from "react-router-dom";
 import { RenderCellValue } from "../../types/table";
 import React from "react";
 
+type Props = {
+  value: RenderCellValue;
+  index?: number;
+  isLink?: boolean;
+  isRed?: boolean;
+  onClick?: () => void;
+};
+
+const renderValue = ({ value, index, isLink, isRed, onClick }: Props) => {
+  if (isLink && index !== undefined) {
+    return (
+      <a
+        key={value.label}
+        href={value.label}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="text-blue-600 underline"
+      >
+        link-{index + 1}
+      </a>
+    );
+  }
+
+  if (value.to) {
+    return (
+      <Link
+        to={value.to}
+        onClick={onClick}
+        className={[
+          "hover:text-blue-600 underline",
+          isRed ? "text-red-500 font-semibold" : "",
+        ].join(" ")}
+      >
+        {value.label}
+      </Link>
+    );
+  }
+
+  return (
+    <span className={isRed ? "text-red-500 font-semibold" : ""}>
+      {value.label}
+    </span>
+  );
+};
+
 type RendarCellProps = {
   value: RenderCellValue | RenderCellValue[];
   isRed?: boolean;
+  isLink?: boolean;
   onClick?: () => void;
 };
 
 export const RenderCell = ({
   value,
   isRed,
+  isLink,
   onClick,
 }: RendarCellProps): React.ReactNode => {
-  const renderValue = (value: RenderCellValue) => {
-    if (value.to) {
-      return (
-        <Link
-          to={value.to}
-          onClick={onClick}
-          className={[
-            "hover:text-blue-600 underline",
-            isRed ? "text-red-500 font-semibold" : "",
-          ].join(" ")}
-        >
-          {value.label}
-        </Link>
-      );
-    }
-
-    return (
-      <span className={isRed ? "text-red-500 font-semibold" : ""}>
-        {value.label}
-      </span>
-    );
-  };
-
   if (Array.isArray(value)) {
     return (
-      <>
+      <div className="flex gap-2">
         {value.map((item, index) => (
           <React.Fragment key={index}>
-            {index > 0 && ", "}
-            {renderValue(item)}
+            {renderValue({ value: item, index, isLink, isRed, onClick })}
           </React.Fragment>
         ))}
-      </>
+      </div>
     );
   }
 
-  return renderValue(value);
+  return renderValue({ value, index: 0, isLink, isRed, onClick });
 };
 
 export default RenderCell;
