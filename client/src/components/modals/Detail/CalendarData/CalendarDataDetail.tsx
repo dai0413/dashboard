0adb454a5cab86148308d22f6f74309373dedf6c
dai0413 <hiroshi01282002@gmail.com>
2026-08-26 17:36:43 +0900

    [Feature] CalendarDetaDetailを整理 #220

---
+++ client/src/components/modals/Detail/CalendarData/CalendarDataDetail.tsx
@@ -0,0 +1,45 @@
+import { useAlert } from "../../../../context/alert-context";
+import Alert from "../../../layout/Alert";
+import { Modal } from "../../../ui";
+import { DisplayListItem } from "../../../../types/detail";
+import FieldList2 from "../../FieldList2";
+
+type CalendarDataDetailProps = {
+  data: DisplayListItem[];
+  title?: string;
+  isOpen: boolean;
+  close: () => void;
+};
+
+const CalendarDataDetail = ({
+  data,
+  title,
+  isOpen,
+  close,
+}: CalendarDataDetailProps) => {
+  const {
+    modal: { alert, resetAlert },
+  } = useAlert();
+
+  return (
+    <Modal
+      isOpen={isOpen}
+      onClose={close}
+      header={
+        <div className="flex items-center gap-x-2 mb-4">
+          <h3 className="text-xl font-semibold text-gray-700">{title}</h3>
+        </div>
+      }
+    >
+      <Alert
+        success={alert?.success || false}
+        message={alert?.message}
+        resetAlert={resetAlert}
+      />
+
+      <FieldList2 data={data} onCLick={close} />
+    </Modal>
+  );
+};
+
+export default CalendarDataDetail;
