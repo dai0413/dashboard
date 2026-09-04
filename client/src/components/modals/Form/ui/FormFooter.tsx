8f8309fdc8ede26a6e5c5e10b03da3a83e6a1768
dai0413 <hiroshi01282002@gmail.com>
2026-09-04 17:39:18 +0900

    [Refactor] FormHeader, FormFooterを調整 #223
    - レンダリングに集中

--- client/src/components/modals/Form/ui/FormFooter.tsx
+++ client/src/components/modals/Form/ui/FormFooter.tsx
@@ -1,86 +1,27 @@
-import { FormStep } from "../../../../types/form";
 import { FormTypeMap } from "../../../../types/models";
-import { FormMode } from "../../../../types/types";
 import { LinkButtonGroup } from "../../../buttons";
 
 type FormFooter<T extends keyof FormTypeMap> = {
-  isTableOpen: boolean;
-  setIsTableOpen: (v: boolean) => void;
-  formSteps: FormStep<T>[];
-  currentStep: number;
-  isEditing: boolean;
-  formMode: FormMode;
-  nextData: () => void;
-  processStep: () => void;
-  prevStep: () => void;
-  setFormPage: (p: number) => void;
+  deny?: {
+    color?: "green" | "red" | "gray" | undefined;
+    text: string;
+    onClick: () => void;
+  };
+
+  approve?: {
+    color?: "green" | "red" | "gray" | undefined;
+    text: string;
+    onClick: () => void;
+  };
 };
 
 export const FormFooter = <T extends keyof FormTypeMap>({
-  isTableOpen,
-  setIsTableOpen,
-  formSteps,
-  currentStep,
-  isEditing,
-  formMode,
-  nextData,
-  processStep,
-  prevStep,
-  setFormPage,
+  deny,
+  approve,
 }: FormFooter<T>) => {
-  const isLastStep = currentStep === formSteps.length - 1;
-
-  if (isTableOpen) {
-    return (
-      <div>
-        <LinkButtonGroup
-          deny={{
-            text: "戻る",
-            color: "red",
-            onClick: () => setIsTableOpen(false),
-          }}
-        />
-      </div>
-    );
-  }
-
-  if (isLastStep && !isEditing) {
-    return (
-      <div>
-        <LinkButtonGroup
-          approve={{
-            text: "次のデータへ",
-            color: "green",
-            onClick: nextData,
-          }}
-        />
-      </div>
-    );
-  }
-
   return (
     <div>
-      <LinkButtonGroup
-        approve={{
-          text:
-            formSteps && currentStep === formSteps.length - 1
-              ? formMode === "create"
-                ? "追加"
-                : "変更"
-              : "次へ",
-          color: "green",
-          onClick: () => {
-            processStep();
-            setFormPage(1);
-          },
-        }}
-        deny={{
-          text: "戻る",
-          color: "red",
-          onClick: prevStep,
-          disabled: currentStep === 0,
-        }}
-      />
+      <LinkButtonGroup approve={approve} deny={deny} />
     </div>
   );
 };
--- client/src/components/modals/Form/ui/FormHeader.tsx
+++ client/src/components/modals/Form/ui/FormHeader.tsx
@@ -1,21 +1,18 @@
 import { AlertStatus } from "../../../../types/alert";
 import { FormStep } from "../../../../types/form";
-import { FormTypeMap, ModelType } from "../../../../types/models";
-import { FormMode } from "../../../../types/types";
+import { FormTypeMap } from "../../../../types/models";
 import Alert from "../../../layout/Alert";
 
 type FormHeader<T extends keyof FormTypeMap> = {
-  modelType: ModelType | null;
-  formMode: FormMode;
-  formSteps: FormStep<T>[];
-  currentStep: number;
+  headingLabel: string;
+  formSteps?: FormStep<T>[];
+  currentStep?: number;
   alert: AlertStatus;
   resetAlert: () => void;
 };
 
 export const FormHeader = <T extends keyof FormTypeMap>({
-  modelType,
-  formMode,
+  headingLabel,
   formSteps,
   currentStep,
   alert,
@@ -24,28 +21,28 @@ export const FormHeader = <T extends keyof FormTypeMap>({
   return (
     <>
       <h3 className="text-xl font-semibold text-gray-700 mb-1">
-        {formMode === "create"
-          ? `${modelType} : 新規データ作成`
-          : `${modelType} : 既存データ編集`}
+        {headingLabel}
       </h3>
 
-      <div>
-        <div className="mb-1 text-sm text-gray-500">
-          ステップ {currentStep + 1} / {formSteps.length}：
-          {formSteps[currentStep].stepLabel || ""}
-        </div>
+      {formSteps && typeof currentStep === "number" && (
+        <div>
+          <div className="mb-1 text-sm text-gray-500">
+            ステップ {currentStep + 1} / {formSteps.length}：
+            {formSteps[currentStep].stepLabel || ""}
+          </div>
 
-        <div className="flex space-x-2">
-          {formSteps.map((_, index) => (
-            <div
-              key={index}
-              className={`flex-1 h-2 rounded-full ${
-                index <= currentStep ? "bg-green-500" : "bg-gray-300"
-              }`}
-            ></div>
-          ))}
+          <div className="flex space-x-2">
+            {formSteps.map((_, index) => (
+              <div
+                key={index}
+                className={`flex-1 h-2 rounded-full ${
+                  index <= currentStep ? "bg-green-500" : "bg-gray-300"
+                }`}
+              ></div>
+            ))}
+          </div>
         </div>
-      </div>
+      )}
 
       <Alert
         success={alert?.success || false}
